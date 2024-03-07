07aa3bd46d9a080ffa24ef64bde0da9020ee8619
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-07 06:27:58 -0800

    Update: Updates task page UI.

--- client/src/pages/Task.jsx
+++ client/src/pages/Task.jsx
@@ -1,15 +1,23 @@
-import { redirect, useNavigate } from "react-router-dom";
+import { redirect, useNavigate, useParams } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import { useDispatch, useSelector } from "react-redux";
 import { setTest } from "../reducers/testReducer";
+import { useState } from "react";
+import Select from "../components/Select";
+import Textarea from "../components/Textarea";
 
 
 const Task = () => {
     const navigate = useNavigate();
-
-    const testValue = useSelector(state => state.test);
     const dispatch = useDispatch();
+
+    const { taskListId, taskId } = useParams();
+    const task = useSelector(state => state.taskManager.taskListIndex.find(item => item.id == taskListId)?.tasks.find(task => task.id === taskId));
+
+    const [data, setData] = useState(task);
+
+    // const testValue = useSelector(state => state.test);
 
     const handleClick = () => {
         // const action = {
@@ -22,8 +30,15 @@ const Task = () => {
         dispatch(setTest('ABCDEFG'));
     }
 
+    function handleFieldChange(e) {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
     function handleGoBack() {
-        navigate('/task-list');
+        navigate(`/task-list/${taskListId}`);
     }
 
     return (
@@ -35,10 +50,20 @@ const Task = () => {
                 <Button title="x" className="w-12" color="white" onClick={handleGoBack} />
             </div>
 
-            <Input type="text" name="name" title="List Name"  />
+            <Input type="text" name="title" title="Title" value={data.title} onChange={handleFieldChange} />
+
+            <div className="flex w-full gap-5">
+                <Input type="date" name="dueDate" title="Due Date" value={data.dueDate} onChange={handleFieldChange} />
+
+                <Select name="priority" title="Priority" value={data.priority} options={{ high: 'High', medium: 'Medium', low: 'Low' }} onChange={handleFieldChange} />
+
+                <Select name="status" title="status" value={data.status} options={{ todo: 'Todo', inProgress: 'In Progress', done: 'Done' }} onChange={handleFieldChange} />
+            </div>
+
+            <Textarea name="description" title="Description" value={data.description} onChange={handleFieldChange} />
 
             <Button title="Delete" className="flex-1" styleType="danger" onClick={handleGoBack} />
-            <Button title="Act" className="flex-1" onClick={handleClick} />
+            {/* <Button title="Act" className="flex-1" onClick={handleClick} /> */}
             <Button title="Save" type="submit" className="flex-1" styleType="primary" onClick={handleGoBack} />
         </form>
     )
