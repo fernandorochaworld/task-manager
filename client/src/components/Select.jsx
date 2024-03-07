d59d9a9253b48ceb662d03d459326e634aeb73c9
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-07 06:25:17 -0800

    New: Add Select and Textarea components.

---
+++ client/src/components/Select.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+const Select = ({type, name, title, value, className, disabled, onChange, options}) => {
+  
+  const [, setVal] = useState(value || '');
+
+  function handleChange(e) {
+    setVal(e.target.value);
+    if (onChange) onChange(e);
+  }
+
+  const myClass = `${className} block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`;
+  return (
+    <div className="w-full">
+      <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">{title}</label>
+      <div className="mt-2">
+        <select id={name} name={name} value={value} required className={myClass} disabled={(disabled)? 'disabled' : ''} onChange={handleChange}>
+          {options && Object.entries(options).map(([key, value]) => (
+            <option key={key} value={key}>{value}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+Select.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.any,
+  options: PropTypes.any,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+export default Select;
