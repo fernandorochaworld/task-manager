07365aa4bf305f7ce55f47a8e01fdb92d783d001
Fernando Rocha <fernandorochaworld@gmail.com>
2024-03-13 15:04:39 -0700

    New: Created browser service.

---
+++ client/src/services/browser-service.js
@@ -0,0 +1,18 @@
+
+const storeUser = (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+}
+
+const getUser = () => {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+const removeUser = () => {
+    localStorage.removeItem('user');
+}
+
+export default {
+    storeUser,
+    getUser,
+    removeUser,
+}
